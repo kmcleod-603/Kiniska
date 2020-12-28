7c69da64abb7454405963687d9349deb841fe298
Kmcleod <58000574+kmcleod-603@users.noreply.github.com>
2020-12-28 14:19:35 -0500

    Update Footer.js

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -6,10 +6,9 @@ import './Footer.scss'
 const Footer = () => {
   return (
     <div className="footer">
-    <p className="footer-link"><a href="mailto:olorequests@sunlife.com"><FontAwesomeIcon icon="envelope" size="sm" /> Product Support</a></p>
+    <p className="footer-link"><a href="mailto:dyi@kiniska.com"><FontAwesomeIcon icon="envelope" size="sm" /> Product Support: Kiniska</a></p>
     <p className="footer-link"><a href="https://xnet.dsgraphics.com/PTI/sunlife/downloads/tutorial.pdf" target="_blank"><FontAwesomeIcon icon="download" size="sm" /> Tutorial</a></p>
-    <p className="footer-link"><a href="https://xnet.dsgraphics.com/PTI/sunlife/downloads/tutorial.pdf" target="_blank"><FontAwesomeIcon icon="video" size="sm" /> Tutorial</a></p>
-    <p className="footer-link"><a href="mailto:helpdesk@dsgraphics.com"><FontAwesomeIcon icon="envelope" size="sm" /> Technical Support</a></p>
+    <p className="footer-link"><a href="mailto:helpdesk@dsgraphics.com"><FontAwesomeIcon icon="envelope" size="sm" /> Technical Support: DSG|UW</a></p>
   </div>
   )
 }
